d9f2193321494677a255ec6df56c2a73df7d1993
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-28 15:47:16 +0530

    forloop

---
+++ Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loops problems");
+            Console.WriteLine("Please enter the number");
+            int x=int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=x; i++)
+            {
+                Console.Write(" {0} ", i);
+            }
+        }
+    }
+}
