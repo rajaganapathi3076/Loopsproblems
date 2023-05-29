cdf742baa0ab627d077eeee789580228f6316e2e
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-29 09:18:35 +0530

    Whileloop3

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -13,20 +13,29 @@ namespace Loops
         {
             // Printing the even numbers in while lops 
 
-            Console.WriteLine("While loops problems 2");
+            Console.WriteLine("While loops problems 3");
             Console.WriteLine("Enter the number");
             int n = int.Parse(Console.ReadLine());
-            int i;
+            int i=0 ;
+            int j=1;
+            int k=0 ;
+            Console.Write("{0} {1}", i, j);
 
 
-            i = 2;
-            while (i <= n)
+                k = i + j;
+            
+            while (k <= n)
             {
-                Console.WriteLine("{0}", i);
-                i = i + 2;
+                Console.Write(" {0} ", k);
+                i++;
+                i = j;
+                j = k;
+                k = i + j;
                
-            } 
-            Console.ReadLine();
+               
+            }
+            Console.ReadKey();
+            
         }
         
     }
